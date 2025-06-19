d808da08534f9d9bd35dabefb87b36ccad247a6a
imcheater124 <shyrynha.andrii@lll.kpi.ua>
2025-06-19 03:05:10 +0300

    complete

---
+++ Nursefreeheal.cs
@@ -0,0 +1,8 @@
+using Terraria.ModLoader;
+
+namespace Nursefreeheal
+{
+    public class Nursefreeheal : Mod
+    {
+    }
+}
---
+++ NursefreehealGlobalNPC.cs
@@ -0,0 +1,37 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Nursefreeheal
+{
+    public class NursefreehealGlobalNPC : GlobalNPC
+    {
+        public override void GetChat(NPC npc, ref string chat)
+        {
+            if (npc.type == NPCID.Nurse)
+            {
+                Player player = Main.LocalPlayer;
+                int missing = player.statLifeMax2 - player.statLife;
+                if (missing > 0)
+                {
+                    player.statLife = player.statLifeMax2;
+                    player.HealEffect(missing, true);
+                }
+                for (int i = 0; i < player.buffType.Length; i++)
+                {
+                    int buff = player.buffType[i];
+                    if (buff != 0
+                        && Main.debuff[buff]
+                        && !BuffID.Sets.NurseCannotRemoveDebuff[buff])
+                    {
+                        player.DelBuff(i);
+                        i--;
+                    }
+                }
+                chat = "[i:58]";
+                return;
+            }
+            base.GetChat(npc, ref chat);
+        }
+    }
+}
